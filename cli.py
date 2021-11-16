e02084033fc0995ebd468966ab82f18070b59f09
Rohith Gangadhara <gangadharrohith6@gmail.com>
2021-11-16 18:18:33 +0530

    ls command clone to me

--- cli.py
+++ cli.py
@@ -1,99 +1,128 @@
+import os
+import subprocess
 from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 
-def err(fun):
-    try:
-        val = fun()
-    except Exception as e:
-        return e
+def help(args):
+    #print('This is hello command.')
+    if args == []:
+        print("1. ls\t\tList all the file and folders.\n\t-l\tList all teh file and folders in a detailed manner.\n\t-al\tList the hidden files along wiht remaining files.")
+        print("2. rm [path]\tRemove the files in the given path.")
+        print("7. quit/exit\tTo stop the execution of the filesystem.")
     else:
-        return val
+        print(
+            f"'help {' '.join(args)}' command not found\nEnter 'help' to know more..")
 
-def hello():
-    print('This is hello command.')
 
 def cat():
     print('This is cat command.')
 
+
 def put():
     print('This is put command.')
 
-def ls():
-    print('This is ls command.')
 
-def rm():
-    print('This is rm command.')
+def ls(args):
+    #print('This is ls command.')
+    temp = 0
+    k = 0
+    if args != []:
+        k = 1
+        temp = subprocess.Popen(['ls', args[0]], stdout=subprocess.PIPE)
+    else:
+        temp = subprocess.Popen('ls', stdout=subprocess.PIPE)
+    output = str(temp.communicate())
+    output = output.split('\'')
+    output = output[1].split('\\')
+    res = []
+    i = 0
+    for line in output:
+        if k == 1:
+            if i != 0:
+                res.append(line[1:])
+            else:
+                res.append(line)
+            i += 1
+        else:
+            if i == 0:
+                res.append(line)
+            elif i < len(output)-1:
+                res.append(line[1:])
+            i += 1
+    for i in res:
+        print(i)
+    return 0
+
+
+def rm(args):
+    #print('This is rm command.')
+    for i in args:
+        if os.path.isfile(i):
+            os.remove(i)
+        elif os.path.isdir(i):
+            print(f"rm: cannot remove '{i}': Is a directory")
+        else:
+            print(f"rm: cannot remove '{i}': No such file or directory")
+
 
 def rmdir():
     print('This is rmdir command.')
 
+
 def mkdir():
     print('This is mkdir command.')
 
-def quit():
-    return -1
+
+def quit(args):
+    if args == []:
+        print('\nGoodbye!!')
+        return -1
+    else:
+        print(
+            f"'quit {' '.join(args)}' not found!!\n Try 'quit' to quit the fs")
+
+
+def exit(args):
+    if args == []:
+        print('\nGoodbye!!')
+        return -1
+    else:
+        print(
+            f"'exit {' '.join(args)}' not found!!\n Try 'exit' to quit the fs")
 
 
 comm = {
-    'hello': hello,
+    'help': help,
     'cat': cat,
     'put': put,
     'ls': ls,
     'rm': rm,
     'rmdir': rmdir,
     'mkdir': mkdir,
-    'quit': quit
+    'quit': quit,
+    'exit': exit
 }
 
 
-
+def err(fun, args):
+    try:
+        val = fun(args)
+    except Exception as e:
+        return e
+    else:
+        return val
 
 
 while 1:
-    user_input = prompt('>',
-                        history=FileHistory('history.txt'),
-                        auto_suggest=AutoSuggestFromHistory(),
-                        )
+    user_input = prompt('>')
 
     try:
-        command = comm[user_input]
-        s = command()
-        if s == -1:
+        userInputList = user_input.split(' ')
+        command = comm[userInputList[0]]
+        val = err(command, userInputList[1:])
+        if val == -1:
             break
     except Exception as e:
-        print(f"{e}: Unknown command")
-
-
-
-
-# if __name__ == '__main__':
-
-#     while 1:
-#         user_input = prompt('>',
-#                             history=FileHistory('history.txt'),
-#                             auto_suggest=AutoSuggestFromHistory(),
-#                         )
-#         command = comm[user_input]
-#         command()
-#         print(user_input)
-        
-
-
-
-
-
-# from cmd import Cmd
- 
-# class MyPrompt(Cmd):
-#    def do_exit(self, inp):
-#         print("Bye")
-#         return True
- 
-#    def do_add(self, inp):
-#         print(type(inp))
-#         print("Adding '{}'".format(inp))
- 
-# MyPrompt().cmdloop()
-# print("after")
+        print(f"{e} is not a proper command\nEnter proper command!!")
