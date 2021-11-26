a3d599c3f8cc2414e2b7840942dcf35c50169daa
J Jeshwanth Reddy <60390772+jeshwanthreddy13@users.noreply.github.com>
2021-11-27 00:22:25 +0530

    Update Hadoop.py

--- Hadoop.py
+++ Hadoop.py
@@ -1,22 +1,84 @@
 import os
 import math
 import random
+import collections
+import itertools
+import multiprocessing
+import operator
+import glob
+import multiprocessing
+import string
+import sys
+sys.path.insert(1,'/Users/jeshwanth/Desktop')
+import mapper
+sys.path.insert(1,'/Users/jeshwanth/Desktop')
+import reducer
+
+class MapReduce(object):
+    
+    def __init__(self, map_func, reduce_func, num_workers=None):#Activated when we create the class object
+        """
+        map_func
+          mapper file Function
+        
+        reduce_func
+           reducer file function
+         
+        num_workers
+          The number of workers to create in the pool. Automatically Defaults to the
+          number of CPUs available on the system.
+        """
+        self.map_func = map_func
+        self.reduce_func = reduce_func
+        self.pool = multiprocessing.Pool(num_workers)#Assigning the number of processes(Concurrent execution) u want, Default is your CPU cores
+    
+    def partition(self, mapped_values):
+        """Organize the mapped values by their key.
+        Returns an unsorted sequence of tuples with a key and a sequence of values.
+        
+        [('money', [1]), ('over', [1, 1]), ('centralized', [1]), ('ledger', [1, 1]), ('paper', [1]), ('through', [1]), ('validators', [1]), ('issuer', [1]), ('proportion', [1]), ('to', [1, 1, 1, 1, 1, 1, 1, 1]), ('issued', [1, 1]), ('first', [1, 1]), ('return', [1]), ('cryptography', [1]), ('get', [1, 1]), ('records', [1, 1]), ('they', [1, 1, 1]), ('not', [1, 1, 1]), ('using', [1]), ('bank', [1]), ('like', [1]), ('generally', [1, 1]), ('crypto', [1, 1, 1]), ('serves', [1]), ('token', [1, 1, 1]), ('stored', [1]), ('each', [1]), ('release', [1]), ('works', [1]), ('schemes', [1]), ('financial', [1]), ('creation', [1]), ('some', [1]), ('authority', [1, 1]), ('individual', [1]), ('are', [1, 1, 1]), ('implemented', [1]), ('stakers', [1]),....)]
+        
+        
+        """
+        partitioned_data = collections.defaultdict(list)#Defaultdict returns a dictionary-like objectwhere a keyerror corresponds to a list in this case
+        for key, value in mapped_values:
+            partitioned_data[key].append(value)
+        return(sorted(partitioned_data.items(),key=lambda kv: kv[0],reverse=False))
+        #return partitioned_data.items()#items() method is used to return the list with all dictionary keys with values.
+    
+    def __call__(self, inputs, chunksize=1):#Activated when we call the object of the class
+        """Process the inputs through the map and reduce functions given.
+        
+        inputs
+          An iterable containing the input data to be processed.
+        
+        chunksize=1
+          The portion of the input data to hand to each worker.  This
+          can be used to tune performance during the mapping phase.
+        """
+        map_responses = self.pool.map(self.map_func, inputs, chunksize=chunksize)#Multithreading the mapper part
+        partitioned_data = self.partition(itertools.chain(*map_responses))#combine all and pass it to partition function
+        reduced_values = self.pool.map(self.reduce_func, partitioned_data)##Multithreading the reducer part
+        return reduced_values
+
+
 #1234
 block_size=64 #in bytes for now
-path_to_datanodes="C:\\Users\\User\\Desktop\\dfs\\DATANODES"
-path_to_namenodes="C:\\Users\\User\\Desktop\\dfs\\NAMENODES"
+path_to_datanodes="/Users/jeshwanth/Desktop/dfs/DATANODES"
+path_to_namenodes="/Users/jeshwanth/Desktop/dfs/NAMENODES"
 replication_factor=3
 num_datanodes=5
 datanode_size=10 #10 blocks in 1 datanode
 sync_period=180 #in miliseconds
-namenode_checkpoints="C:\\Users\\User\\Desktop\\dfs\\NAMENODES\\CHECKPOINTS"
+namenode_checkpoints="/Users/jeshwanth/Desktop/dfs/NAMENODES/CHECKPOINTS"
 #1234
 #step1
 #get input file, calculate size of file, calculate no. of blocks required
 def compute_file_size():
-    global file_size,number_of_blocks,file_name
+    global file_size,file_name,number_of_blocks;
     file_name=input("enter the file name along with extension")
-    file_size=os.path.getsize(file_name)
+    file_size = os.path.getsize(file_name)
+    #print(file_size)
     number_of_blocks=math.ceil(file_size/(block_size-1))
 
 
@@ -28,7 +90,7 @@ def create_datanodes():
     datanodes_path_list=[]
     for i in range(1,num_datanodes+1):
         datanode_name="datanode"+str(i)+".txt"
-        datanode_X=path_to_datanodes+str("\\")+datanode_name
+        datanode_X=path_to_datanodes+str("/")+datanode_name
         fp=open(datanode_X,"w")
         datanodes_path_list.append([datanode_X,int(0)])
         fp.close()
@@ -37,7 +99,7 @@ def create_datanodes():
 #do the same for namenode
 def create_namenode():
     global namenode_path
-    namenode_path=str(path_to_namenodes+"\\namenode.txt")
+    namenode_path=str(path_to_namenodes+"/namenode.txt")
     fp=open(namenode_path,"w")
     fp.close()
 
@@ -49,7 +111,7 @@ def NAMENODE():
 
     fp_namenode=open(namenode_path,"w")
     fp_user_file=open(file_name,"r")
-    fp_namenode.write(file_name+"###"+str(number_of_blocks)+"\n")
+    #fp_namenode.write(file_name+"###"+str(number_of_blocks)+"\n") #Can't Use this and parse the string from the text file so i am commenting it out
     for i in range(1,number_of_blocks+1):
         flag=True
         while(flag):
@@ -58,7 +120,7 @@ def NAMENODE():
             offset=path_offset[1]#check if data node is full, put of 1f(offset<=block_size*10)
             if(offset<=(block_size*datanode_size)-block_size):
                 flag=False
-        fp_namenode.write("Block"+str(i)+" "+str(path)+" "+"offset: "+str(offset)+" "+"rep_factor: 0"+"\n")
+        fp_namenode.write("Block: "+str(i)+" "+str(path)+" "+"offset: "+str(offset)+" "+"rep_factor: 0"+"\n")
         line=""
         for x in range(block_size-1):
             try:
@@ -81,7 +143,7 @@ def NAMENODE():
                 offset_new_x=path_offset_new[1]
                 if(offset_new_x<=(block_size*datanode_size)-block_size):
                     BLOCK(path_new,line)
-                    fp_namenode.write("Block"+str(i)+" "+str(path_new) +" "+ "offset: " + str(offset_new_x) + " " + "rep_factor: "+str(rep) + "\n")
+                    fp_namenode.write("Block: "+str(i)+" "+str(path_new) +" "+ "offset: " + str(offset_new_x) + " " + "rep_factor: "+str(rep) + "\n")
                     offset_new_x=int(offset_new_x)+len(line)+1
                     datanodes_path_list[datanodes_path_list.index(path_offset_new)] = [path_new, offset_new_x]
                     rep+=1
@@ -97,12 +159,112 @@ def BLOCK(path,line):
     fp_datanode_x=open(path,"a")
     fp_datanode_x.write(line+"\n")
     fp_datanode_x.close()
+    
+def Mapper(line):#Mapper
+   
+    
+      
+    return mapper.doo(line);
+ 
+      
 
+ 
+  
+
+    
+
+
+def Reducer(item):#Reducer
+    
+    return(reducer.doo(item));
+    
+def Pass_To_Mapper(path,offset):
+  
+  try:
+      fhand = open(path, "r")
+  except OSError:
+    print("Could not open/read file:", path);
+
+
+  fhand.read(int(offset))
+  line=""
+  for x in range(block_size-1):
+    try:
+     line=line+fhand.read(1)
+    except:
+      break
+  fhand.seek(0);
+  #Calling the class object which in turn invokes __call__ Function
+ 
+  return line;
+
+
+if __name__ == '__main__':#Using This for better code understanding :-)
 #call only if HDFS space is bigger than the file to be stored
-compute_file_size()
-if(math.floor(num_datanodes*block_size*datanode_size/replication_factor)>=file_size):
+ block_size=300 #in bytes for now
+ path_to_datanodes="/Users/jeshwanth/Desktop/dfs/DATANODES"
+ path_to_namenodes="/Users/jeshwanth/Desktop/dfs/NAMENODES"
+ replication_factor=3
+ num_datanodes=20
+ datanode_size=20 #10 blocks in 1 datanode
+ sync_period=180 #in miliseconds
+ namenode_checkpoints="/Users/jeshwanth/Desktop/dfs/NAMENODES/CHECKPOINTS"
+
+ compute_file_size()
+ if(math.floor(num_datanodes*block_size*datanode_size/replication_factor)>=file_size):
     create_datanodes()
     create_namenode()
     NAMENODE()
-else:
+ else:
     print("File size too big for HDFS to compute")
+
+
+
+ mappper = MapReduce(Mapper, Reducer)#returning a class object to mapper
+ Width = 4;#After removing irrelevent Widthords(Characters) the size of the Widthidth required
+ Matrix = [[0 for x in range(Width)] for y in range(number_of_blocks*replication_factor)]
+ 
+ try:
+      file1 = open('/Users/jeshwanth/Desktop/dfs/NAMENODES/namenode.txt', 'r')
+ except OSError:
+    print("Could not open NameNode ....Terminating");
+    sys.exit("File not Open Error");
+
+ Lines = file1.readlines();
+ count = 0;
+ for line in Lines:
+    line = line.strip();
+    line_array = line.split(' ');
+    line_array.pop(0);
+    line_array.pop(2);
+    line_array.pop(3);
+    Matrix[count] = line_array;
+    count += 1;
+ #print(Matrix)
+ Block_Flag=[0 for x in range(number_of_blocks)];
+#print(Block_Flag)
+ Accessed_Blocks =[]
+ input=[];
+#print(Matrix[0][0]);
+ for i in range(number_of_blocks*replication_factor):
+  #print(Matrix[i][0])
+   if(int(Matrix[i][0]) in Accessed_Blocks ):
+    continue;
+   elif(int(Matrix[i][0]) not in Accessed_Blocks):
+    Accessed_Blocks.append(int(Matrix[i][0]));
+    Block_Flag[int(Matrix[i][0]) - 1] = 1;
+    input.append(Pass_To_Mapper(Matrix[i][1],Matrix[i][2]));
+    word_counts = mappper(input)
+ if(0 in Block_Flag):
+    sys.exit("Block Missing even though replication is used,Reasons:-Block maybe missing (or) Block is corrupt (Or) Read write premissions are not given");
+     #Calling the class object which in turn invokes __call__ Function
+ 
+    
+ longest = max(len(word) for word, count in word_counts)#For having symmetrical space all through the output by selecting the longest word
+ for word, count in word_counts:#Iterating through each tuple
+    print('%-*s: %5s' % (longest+1, word, count));#The "%-*s" indicates a left trailing space whose value will be passes by longest+1 in the print statement
+
+
+   #mappper = MapReduce(Mapper, Reducer)#returning a class object to mapper
+   
+
